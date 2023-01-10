8164d2d2fc7cf42ba013531043ca7df25c30de19
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-11 00:54:50 +0900

    Post Model And Relationship With User Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,23 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $role = Role::create([
+            'name' => 'author',
+        ]);
+
+        $user = $role->users()->create([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'status' => 1,
+        ]);
+
+        $user->posts()->create([
+            'title' => 'This is title',
+            'slug' => 'This is slug',
+            'excerpt' => 'This is excerpt',
+            'body' => 'This is content',
+        ]);
     }
 }
