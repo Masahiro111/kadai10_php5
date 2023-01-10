e572b9cb4444cab1306a2249673abdeab2e8f653
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-11 01:58:48 +0900

    Tag Model And Relationship With Post Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Role;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -25,28 +26,62 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $role = Role::create([
+        // ロール名の初期設定
+        $role1 = Role::query()->create([
+            'name' => 'user',
+        ]);
+        $role2 = Role::query()->create([
             'name' => 'author',
         ]);
+        $role3 = Role::query()->create([
+            'name' => 'admin',
+        ]);
 
+        // タグ名の初期設定
+        $tag1 = Tag::query()->create([
+            'name' => 'php',
+        ]);
+        $tag2 = Tag::query()->create([
+            'name' => 'c++',
+        ]);
+        $tag3 = Tag::query()->create([
+            'name' => 'ruby',
+        ]);
+
+        // カテゴリ名の初期設定
         $category = Category::create([
             'name' => 'Education',
             'slug' => 'education'
         ]);
 
-        $user = $role->users()->create([
-            'name' => 'test',
+        // ユーザー情報の登録
+        $user = $role2->users()->create([
+            'name' => 'masahiro',
             'email' => 'test@example.com',
             'password' => Hash::make('password'),
             'status' => 1,
         ]);
 
-        $user->posts()->create([
+        // ユーザー情報の登録
+        $admin = $role3->users()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'status' => 1,
+        ]);
+
+        // 記事情報の登録
+        $post = $user->posts()->create([
             'title' => 'This is title',
             'slug' => 'This is slug',
             'excerpt' => 'This is excerpt',
             'body' => 'This is content',
             'category_id' => 1,
+        ]);
+
+        // タグと記事のリレーション設定
+        $post->tags()->attach([
+            $tag1->id, $tag2->id, $tag3->id
         ]);
     }
 }
