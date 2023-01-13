820b4de06dbe64fde7798180490a59c96b999088
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-13 20:05:30 +0900

    ホームページの記事表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::query()
+            ->withCount('comments')
+            ->get();
+
+        return view('home', compact('posts'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,9 +23,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/post', function () {
     return view('post');
