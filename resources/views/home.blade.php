e539a80cc107e3b400a934c36a5896ae9db3b6d2
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-16 19:55:34 +0900

    個別記事の表示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,40 +33,11 @@
             <!-- SIDEBAR: start -->
             <div class="col-md-4 animate-box">
                 <div class="sidebar">
-                    <div class="side">
-                        <h3 class="sidebar-heading">Categories</h3>
-                        <div class="block-24">
-                            <ul>
-                                @foreach ($categories as $category)
-                                <li><a href="#">{{ $category->name }} <span>{{ $category->posts_count }}</span></a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="side">
-                        <h3 class="sidebar-heading">Recent Blog</h3>
-                        @foreach ($recent_posts as $recent_post)
-                        <div class="f-blog">
-                            <a href="blog.html" class="blog-img" style="background-image: url({{ asset('storage/' . $recent_post->image->path ) }});">
-                            </a>
-                            <div class="desc">
-                                <p class="admin"><span>{{ $recent_post->created_at->diffForHumans() }}</span></p>
-                                <h2><a href="blog.html">{{ Str::limit( $recent_post->title, 20, '...') }}</a></h2>
-                                <p>{{ $recent_post->excerpt }}</p>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                    <div class="side">
-                        <h3 class="sidbar-heading">Tags</h3>
-                        <div class="block-26">
-                            <ul>
-                                @foreach ($tags as $tag)
-                                <li><a href="#">{{ $tag->name }}</a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
+                    <x-blog.side-categories :categories="$categories" />
+
+                    <x-blog.side-recent-posts :recentPosts="$recent_posts" />
+
+                    <x-blog.side-tags :tags="$tags" />
                 </div>
             </div>
         </div>
