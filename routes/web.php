1ee619d1411d6b1660221257880a24102d99959e
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-18 02:24:45 +0900

    Admin パネルの作成

--- routes/web.php
+++ routes/web.php
@@ -3,35 +3,18 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-// Route::get('/post', function () {
-//     return view('post');
-// })->name('post');
 
 Route::get('/post/{post:slug}', [PostController::class, 'show'])
     ->name('posts.show');
@@ -40,10 +23,6 @@ Route::post('/post/{post:slug}', [PostController::class, 'addComment'])
     ->name('posts.add_comment');
 
 Route::get('/about', AboutController::class)->name('about');
-
-// Route::get('/contact', function () {
-//     return view('contact');
-// })->name('contact');
 
 Route::get('/contact', [ContactController::class, 'create'])
     ->name('contact.create');
@@ -59,6 +38,13 @@ Route::get('/categories', [CategoryController::class, 'index'])
 
 Route::get('/tags/{tag:name}', [TagController::class, 'show'])
     ->name('tags.show');
+
+
+
+//  ADMIN  ROUTES  ------------------------------------------------------------------------------------
+
+Route::get('/admin', [DashboardController::class, 'index'])
+    ->name('admin.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
