b582050a624baa1e549071507532da9d90baf06a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-01-20 00:36:45 +0900

    fix

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $table = 'comments';
+
     protected $fillable = [
         'the_comment',
         'user_id',
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,5 +9,7 @@ class Contact extends Model
 {
     use HasFactory;
 
+    protected $table = 'contacts';
+
     protected $guarded = [];
 }
